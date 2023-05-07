25b8baacba8d8c502dffeb6ad6ab1f1f576b17f8
Nimra Amjad <74602690+Nimra-Amjad@users.noreply.github.com>
2023-05-07 21:08:28 +0500

    Add files via upload

---
+++ api/middleware/check-auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    console.log(token);
+    const verify = jwt.verify(token, "This is dummy text");
+    console.log(verify);
+    if (verify.userType == "admin") {
+      next();
+    } else {
+      return res.status(401).json({
+        msg: "not admin",
+      });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      msg: "Invalid token",
+    });
+  }
+};
